f8e7a3d5b66565c78fd11588c8fdc06717d81f9a
Nadav Shatz <nadavshatz@gmail.com>
2017-10-18 15:37:14 +0300

    Remove bower

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,14 +3,6 @@ module.exports = {
   scenarios: [
     {
       name: 'ember-lts-2.8',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#lts-2-8'
-        },
-        resolutions: {
-          'ember': 'lts-2-8'
-        }
-      },
       npm: {
         devDependencies: {
           'ember-source': null
@@ -27,14 +19,6 @@ module.exports = {
     },
     {
       name: 'ember-release',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#release'
-        },
-        resolutions: {
-          'ember': 'release'
-        }
-      },
       npm: {
         devDependencies: {
           'ember-source': null
@@ -43,14 +27,6 @@ module.exports = {
     },
     {
       name: 'ember-beta',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#beta'
-        },
-        resolutions: {
-          'ember': 'beta'
-        }
-      },
       npm: {
         devDependencies: {
           'ember-source': null
@@ -59,14 +35,6 @@ module.exports = {
     },
     {
       name: 'ember-canary',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#canary'
-        },
-        resolutions: {
-          'ember': 'canary'
-        }
-      },
       npm: {
         devDependencies: {
           'ember-source': null
